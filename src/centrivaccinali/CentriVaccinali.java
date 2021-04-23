47860f3aa9aa4c87c0e6b9aa9518f7cf470722d8
Kryoft <fulmine.cris@gmail.com>
2021-04-23 18:27:27 +0200

    Ristrutturazione FileSystem

--- src/centrivaccinali/CentriVaccinali.java
+++ src/centrivaccinali/CentriVaccinali.java
@@ -1,9 +1,16 @@
+/*
+ * Davide Spinelli, num_matricola, CO
+ * Cristian Corti, 744359, CO
+ * Manuel Marceca, num_matricola, CO
+ */
+
 package centrivaccinali;
 
 public class CentriVaccinali {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
--- src/cittadini/Cittadini.java
+++ src/cittadini/Cittadini.java
@@ -1,10 +1,17 @@
+/*
+ * Davide Spinelli, num_matricola, CO
+ * Cristian Corti, 744359, CO
+ * Manuel Marceca, num_matricola, CO
+ */
+
 package cittadini;
 
 public class Cittadini {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
