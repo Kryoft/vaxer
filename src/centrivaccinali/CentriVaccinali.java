a72cfbff99a9f6aa33319d4a9a9f5bbfdcec2f58
D-000 <davidespinelli.io@gmail.com>
2021-04-23 15:54:22 +0200

    Package e classi create

---
+++ src/centrivaccinali/CentriVaccinali.java
@@ -0,0 +1,10 @@
+package centrivaccinali;
+
+public class CentriVaccinali {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/cittadini/Cittadini.java
@@ -0,0 +1,10 @@
+package cittadini;
+
+public class Cittadini {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
